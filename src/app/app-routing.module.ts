aef787a15ea8d303996306471f8dfef6b9dc7251
Gergő Pattantyus <pattantyus.gerg@gmail.com>
2020-09-21 22:10:59 +0200

    App ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { ReceiverComponent } from './receiver/receiver.component';
+import { SenderComponent } from './sender/sender.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sender', component: SenderComponent},
+  {path: '', component: LandingComponent},
+  {path: 'receiver', component: ReceiverComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SenderComponent } from './sender/sender.component';
+import { ReceiverComponent } from './receiver/receiver.component';
+import { ClipboardModule } from 'ngx-clipboard';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { LandingComponent } from './landing/landing.component';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SenderComponent,
+    ReceiverComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    FormsModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
